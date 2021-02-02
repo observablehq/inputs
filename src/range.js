8834ca30a9e3305ea8712571b5fc8997bff028f8
Mike Bostock <mbostock@gmail.com>
2021-02-02 09:12:57 -0800

    don’t mutate transform.invert

--- src/range.js
+++ src/range.js
@@ -3,13 +3,13 @@ import {length} from "./css.js";
 import {preventDefault} from "./event.js";
 import {formatNumber} from "./format.js";
 import {maybeLabel} from "./label.js";
-import {solve} from "./newton.js";
 
-const linear = Object.assign(x => x, { invert: x => x });
+const epsilon = 1e-6;
+const identity = x => x;
 
 export function Range([min, max] = [0, 1], {
   format = d => formatNumber(d).replace(/,/g, ""), // number is strict!
-  transform = linear,
+  transform = identity,
   label = "",
   value,
   step,
@@ -24,22 +24,20 @@ export function Range([min, max] = [0, 1], {
       <input type=range name=range oninput=${onrange} disabled=${disabled}>
     </div>
   </form>`;
-
   if (typeof transform !== "function") throw new TypeError("transform is not a function");
-  if (typeof transform.invert !== "function") transform.invert  = x => solve(transform, x, x);
-
+  const invert = transform.invert === undefined ? solver(transform) : transform.invert;
+  if (typeof invert !== "function") throw new TypeError("transform.invert is not a function");
   const {range, number} = form.elements;
-  range.min = transform.invert(number.min = min = +min);
-  range.max = transform.invert(number.max = max = +max);
+  range.min = invert(number.min = min = +min);
+  range.max = invert(number.max = max = +max);
   number.step = range.step = step === undefined ? "any" : step = +step;
   number.value = value === undefined ? (min + max) / 2 : +value;
-  range.value = transform.invert(number.value);
-  
+  range.value = invert(number.value);
   function onrange() {
     number.value = format(value = transform(range.valueAsNumber));
   }
   function onnumber() {
-    range.value = transform.invert(value = number.valueAsNumber);
+    range.value = invert(value = number.valueAsNumber);
   }
   onrange();
   return Object.defineProperty(form, "value", {
@@ -51,4 +49,24 @@ export function Range([min, max] = [0, 1], {
       onnumber();
     }
   });
+}
+
+function solver(f) {
+  if (f === identity) return identity;
+  if (f === Math.sqrt) return x => x * x;
+  if (f === Math.log) return Math.exp;
+  return x => solve(f, x, x);
+}
+
+function solve(f, y, x) {
+  let steps = 100, delta, f0, f1;
+  x = x === undefined ? 0 : +x;
+  y = +y;
+  do {
+    f0 = f(x);
+    f1 = f(x + epsilon);
+    if (f0 === f1) f1 = f0 + epsilon;
+    x -= delta = (-1 * epsilon * (f0 - y)) / (f0 - f1);
+  } while (steps-- > 0 && Math.abs(delta) > epsilon);
+  return steps < 0 ? NaN : x;
 }
