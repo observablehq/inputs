5c1269a555a8e88ea870046724e326444057c6f8
Philippe Rivière <fil@rezo.net>
2021-02-02 15:53:27 +0100

    non-linear sliders
    You can pass a function with an invert, if you want, or leave it to newton
    
    closes #20
    
    demo https://observablehq.com/@data-workflows/non-linear-range

--- src/range.js
+++ src/range.js
@@ -3,9 +3,13 @@ import {length} from "./css.js";
 import {preventDefault} from "./event.js";
 import {formatNumber} from "./format.js";
 import {maybeLabel} from "./label.js";
+import {solve} from "./newton.js";
+
+const linear = Object.assign(x => x, { invert: x => x });
 
 export function Range([min, max] = [0, 1], {
   format = d => formatNumber(d).replace(/,/g, ""), // number is strict!
+  transform = linear,
   label = "",
   value,
   step,
@@ -13,23 +17,34 @@ export function Range([min, max] = [0, 1], {
   placeholder,
   width
 } = {}) {
-  if (typeof format !==  "function") throw new TypeError("format is not a function");
+  if (typeof format !== "function") throw new TypeError("format is not a function");
   const form = html`<form class=__ns__ onsubmit=${preventDefault}>
     ${maybeLabel(label)}<div class=__ns__-input style=${{width: length(width)}}>
       <input type=number name=number placeholder=${placeholder} oninput=${onnumber} disabled=${disabled}>
       <input type=range name=range oninput=${onrange} disabled=${disabled}>
     </div>
   </form>`;
+
+  if (typeof transform !== "function") throw new TypeError("transform is not a function");
+  if (typeof transform.invert !== "function") transform.invert  = x => solve(transform, x, x);
+
   const {range, number} = form.elements;
-  number.min = range.min = min = +min;
-  number.max = range.max = max = +max;
+  range.min = transform.invert(number.min = min = +min);
+  range.max = transform.invert(number.max = max = +max);
   number.step = range.step = step === undefined ? "any" : step = +step;
-  number.value = range.value = value === undefined ? (min + max) / 2 : +value;
+  if (value === undefined) {
+    range.value = (+range.min + +range.max) / 2;
+    number.value = transform(range.valueAsNumber);
+  } else {
+    number.value = +value;
+    range.value = transform.invert(number.value);
+  }
+  
   function onrange() {
-    number.value = format(value = range.valueAsNumber);
+    number.value = format(value = transform(range.valueAsNumber));
   }
   function onnumber() {
-    range.value = value = number.valueAsNumber;
+    range.value = transform.invert(value = number.valueAsNumber);
   }
   onrange();
   return Object.defineProperty(form, "value", {
@@ -37,8 +52,8 @@ export function Range([min, max] = [0, 1], {
       return value;
     },
     set(v) {
-      range.value = v;
-      onrange();
+      number.value = v;
+      onnumber();
     }
   });
 }
