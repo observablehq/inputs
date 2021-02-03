157e4ae81cd159b7cf21dd8dd60169aea526f0b4
Mike Bostock <mbostock@gmail.com>
2021-02-03 14:50:30 -0800

    remove Input.bind in favor of bind

--- src/input.js
+++ src/input.js
@@ -1,11 +1,6 @@
-import {bind} from "./bind.js";
-
 export class Input extends EventTarget {
   constructor(value) {
     super();
     this.value = value;
-  }
-  bind(input, invalidation) {
-    return bind(input, this, invalidation);
   }
 }
