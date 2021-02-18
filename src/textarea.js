8eee1826ffe10cf4ea01ed665e895590a75411b9
Mike Bostock <mbostock@gmail.com>
2021-02-18 15:21:57 -0800

    tweak textarea styles

--- src/textarea.js
+++ src/textarea.js
@@ -1,5 +1,6 @@
 import {html} from "htl";
-import {length} from "./css.js";
+import {maybeWidth} from "./css.js";
+// import {length} from "./css.js";
 import {dispatchInput, preventDefault} from "./event.js";
 import {stringify} from "./format.js";
 import {maybeLabel} from "./label.js";
@@ -7,29 +8,30 @@ import {maybeLabel} from "./label.js";
 export function Textarea({
   label,
   value = "",
+  validate, // TODO
   placeholder,
   spellcheck,
   submit,
-  rows,
-  cols,
-  fontFamily,
-  readonly,
-  resize,
+  rows = 3,
   minlength,
   maxlength,
+  required = minlength > 0,
   disabled,
   width
 } = {}) {
   submit = submit === true ? "Submit" : submit || null;
-  width = width !== undefined ? length(width) : cols !== undefined ? `${1 + cols * 0.61}em` : undefined;
-  fontFamily = fontFamily === undefined ? null : fontFamily;
   const button = submit ? html`<button type=submit disabled>${submit}` : null;
-  const form = html`<form class=__ns__ onsubmit=${onsubmit} style=${{width: "640px", maxWidth: "100%"}}>
-    ${maybeLabel(label)}<div class=__ns__-textarea style=${{width}}>
-      <textarea name=text disabled=${disabled} rows=${rows} cols=${cols} readonly=${readonly} minlength=${minlength} maxlength=${maxlength} spellcheck=${spellcheck === undefined ? false : spellcheck + ""} placeholder=${placeholder} oninput=${oninput} style=${{width, resize, fontFamily}}>${stringify(value)}</textarea>${button}
+  const form = html`<form class="__ns__ __ns__-textarea" onsubmit=${onsubmit} style=${maybeWidth(width)}>
+    ${maybeLabel(label)}<div>
+      <textarea name=text disabled=${disabled} required=${required} rows=${rows} minlength=${minlength} maxlength=${maxlength} spellcheck=${spellcheck === undefined ? false : spellcheck + ""} placeholder=${placeholder} onkeydown=${onkeydown} oninput=${oninput} style=${{width}}>${stringify(value)}</textarea>${button}
     </div>
   </form>`;
   const {text} = form.elements;
+  function onkeydown(event) {
+    if (submit && event.key === "Enter" && (event.metaKey || event.ctrlKey)) {
+      return onsubmit(event);
+    }
+  }
   function onsubmit(event) {
     if (submit) {
       value = text.value;
