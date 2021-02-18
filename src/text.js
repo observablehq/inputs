fc1b316e7a7b4e7897cde7dec7cf94a27cc312ea
Mike Bostock <mbostock@gmail.com>
2021-02-18 15:46:52 -0800

    createText helper

--- src/text.js
+++ src/text.js
@@ -5,31 +5,13 @@ import {dispatchInput, preventDefault} from "./event.js";
 import {stringify} from "./format.js";
 import {maybeLabel} from "./label.js";
 
-export function Text({
-  label,
-  type = "text",
-  value = "",
-  validate = checkValidity,
-  placeholder,
-  pattern,
-  spellcheck,
-  submit,
-  minlength,
-  maxlength,
-  required = minlength > 0,
-  datalist,
-  disabled,
-  width
-} = {}) {
+export function createText(form, input, {value = "", submit, validate = checkValidity} = {}) {
   submit = submit === true ? "Submit" : submit || null;
-  const [list, listId] = maybeDatalist(datalist);
   const button = submit ? html`<button type=submit disabled>${submit}` : null;
-  const input = html`<input type=${type} name=text list=${listId} disabled=${disabled} required=${required} minlength=${minlength} maxlength=${maxlength} pattern=${pattern} spellcheck=${spellcheck === undefined ? false : spellcheck + ""} placeholder=${placeholder} value=${stringify(value)} oninput=${oninput}>`;
-  const form = html`<form class=__ns__ onsubmit=${onsubmit} style=${maybeWidth(width)}>
-    ${maybeLabel(label, input)}<div class=__ns__-input>
-      ${input}${button}
-    </div>${list}
-  </form>`;
+  if (submit) input.after(button);
+  input.value = stringify(value);
+  form.onsubmit = onsubmit;
+  input.oninput = oninput;
   function update() {
     if (validate(input)) {
       value = input.value;
@@ -56,9 +38,33 @@ export function Text({
       return value;
     },
     set(v) {
-      input.value = value = v;
+      input.value = value = stringify(v);
     }
   });
+}
+
+export function Text({
+  label,
+  type = "text",
+  placeholder,
+  pattern,
+  spellcheck,
+  minlength,
+  maxlength,
+  required = minlength > 0,
+  datalist,
+  disabled,
+  width,
+  ...options
+} = {}) {
+  const [list, listId] = maybeDatalist(datalist);
+  const input = html`<input type=${type} name=text list=${listId} disabled=${disabled} required=${required} minlength=${minlength} maxlength=${maxlength} pattern=${pattern} spellcheck=${spellcheck === undefined ? false : spellcheck + ""} placeholder=${placeholder}>`;
+  const form = html`<form class=__ns__ style=${maybeWidth(width)}>
+    ${maybeLabel(label, input)}<div class=__ns__-input>
+      ${input}
+    </div>${list}
+  </form>`;
+  return createText(form, input, options);
 }
 
 function checkValidity(input) {
--- src/textarea.js
+++ src/textarea.js
@@ -1,59 +1,30 @@
 import {html} from "htl";
 import {maybeWidth} from "./css.js";
-// import {length} from "./css.js";
-import {dispatchInput, preventDefault} from "./event.js";
-import {stringify} from "./format.js";
 import {maybeLabel} from "./label.js";
+import {createText} from "./text.js";
 
 export function Textarea({
   label,
-  value = "",
-  validate, // TODO
   placeholder,
   spellcheck,
-  submit,
   rows = 3,
   minlength,
   maxlength,
   required = minlength > 0,
   disabled,
-  width
+  width,
+  ...options
 } = {}) {
-  submit = submit === true ? "Submit" : submit || null;
-  const button = submit ? html`<button type=submit disabled>${submit}` : null;
-  const form = html`<form class="__ns__ __ns__-textarea" onsubmit=${onsubmit} style=${maybeWidth(width)}>
-    ${maybeLabel(label)}<div>
-      <textarea name=text disabled=${disabled} required=${required} rows=${rows} minlength=${minlength} maxlength=${maxlength} spellcheck=${spellcheck === undefined ? false : spellcheck + ""} placeholder=${placeholder} onkeydown=${onkeydown} oninput=${oninput} style=${{width}}>${stringify(value)}</textarea>${button}
+  const input = html`<textarea name=text disabled=${disabled} required=${required} rows=${rows} minlength=${minlength} maxlength=${maxlength} spellcheck=${spellcheck === undefined ? false : spellcheck + ""} placeholder=${placeholder} onkeydown=${onkeydown} style=${{width}}>`;
+  const form = html`<form class="__ns__ __ns__-textarea" style=${maybeWidth(width)}>
+    ${maybeLabel(label, input)}<div>
+      ${input}
     </div>
   </form>`;
-  const {text} = form.elements;
   function onkeydown(event) {
-    if (submit && event.key === "Enter" && (event.metaKey || event.ctrlKey)) {
-      return onsubmit(event);
+    if (options.submit && event.key === "Enter" && (event.metaKey || event.ctrlKey)) {
+      return form.onsubmit(event);
     }
   }
-  function onsubmit(event) {
-    if (submit) {
-      value = text.value;
-      button.disabled = true;
-      dispatchInput(event);
-    }
-    preventDefault(event);
-  }
-  function oninput(event) {
-    if (submit) {
-      button.disabled = value === text.value;
-      event.stopPropagation();
-    } else {
-      value = text.value;
-    }
-  }
-  return Object.defineProperty(form, "value", {
-    get() {
-      return value;
-    },
-    set(v) {
-      text.value = value = v;
-    }
-  });
+  return createText(form, input, options);
 }
