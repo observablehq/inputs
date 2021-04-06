49bb1dba12044f9545e875e46753219f5b14e02c
Mike Bostock <mbostock@gmail.com>
2021-04-06 16:10:02 -0700

    fix #125; narrow table scrollbar

--- src/table.js
+++ src/table.js
@@ -53,8 +53,8 @@ export function Table(
   const tbody = html`<tbody>`;
   const tr = html`<tr><td><input type=checkbox></td>${columns.map(() => html`<td>`)}`;
   const theadr = html`<tr><th><input type=checkbox onclick=${reselectAll}></th>${columns.map((column) => html`<th title=${column} onclick=${event => resort(event, column)}><span></span>${column}</th>`)}</tr>`;
-  const root = html`<div class="__ns__ __ns__-table" id=${id} style="max-height: ${height}px;">
-  <table style=${{tableLayout: layout, width: typeof width === "string" || typeof width === "number" ? length(width) : undefined}}>
+  const root = html`<div class="__ns__ __ns__-table" id=${id} style=${{maxHeight: `${height}px`, width: typeof width === "string" || typeof width === "number" ? length(width) : undefined}}>
+  <table style=${{tableLayout: layout}}>
     <thead>${N || columns.length ? theadr : null}</thead>
     ${tbody}
   </table>
