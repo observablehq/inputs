4e1586ba51e0f587da607c5bbfb440b2a4e18b0a
Mike Bostock <mbostock@gmail.com>
2021-09-19 08:01:36 -0700

    Update datalist.js

--- src/datalist.js
+++ src/datalist.js
@@ -4,6 +4,6 @@ import {newId} from "./id.js";
 export function maybeDatalist(datalist) {
   if (datalist === undefined) return [null, null];
   const listId = newId();
-  const list = html`<datalist id=${listId}>${Array.from(datalist, value => html`<option value=${""+ value}>`)}`;
+  const list = html`<datalist id=${listId}>${Array.from(datalist, value => html`<option value=${value + ""}>`)}`;
   return [list, listId];
 }
