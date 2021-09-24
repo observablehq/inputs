424dd325cc5949981115c010a1dd2d2d991f2599
Mike Bostock <mbostock@gmail.com>
2021-09-24 07:32:17 -0700

    better string coercion (#179)

--- src/css.js
+++ src/css.js
@@ -1,5 +1,5 @@
 export function length(x) {
-  return x == null ? null : typeof x === "number" ? `${x}px` : x + "";
+  return x == null ? null : typeof x === "number" ? `${x}px` : `${x}`;
 }
 
 export function maybeWidth(width) {
