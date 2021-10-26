bb785a18d16a40e6a0708c4a4666258651ce45ee
Mike Bostock <mbostock@gmail.com>
2021-10-25 21:07:26 -0700

    form set

--- src/form.js
+++ src/form.js
@@ -5,19 +5,41 @@ export function form(inputs) {
 }
 
 function arrayForm(inputs) {
-  inputs = [...inputs];
-  const oninput = () => form.value = inputs.map(({value}) => value);
+  inputs = [...inputs]; // defensive copy
+  let value; // lazily constructed
   const form = html`<form>${inputs}`;
-  form.addEventListener("input", oninput);
-  oninput();
-  return form;
+  form.addEventListener("input", () => value = undefined);
+  return Object.defineProperty(form, "value", {
+    get() {
+      return value === undefined
+        ? (value = inputs.map(({value}) => value))
+        : value;
+    },
+    set(v = []) {
+      value = undefined;
+      for (let i = 0, n = inputs.length; i < n; ++i) {
+        inputs[i].value = v[i];
+      }
+    }
+  });
 }
 
 function objectForm(inputs) {
-  inputs = Object.entries(inputs);
-  const oninput = () => form.value = Object.fromEntries(inputs.map(([name, {value}]) => [name, value]));
-  const form = html`<form>${inputs.map(([, input]) => input)}`;
-  form.addEventListener("input", oninput);
-  oninput();
-  return form;
+  inputs = {...inputs}; // defensive copy
+  let value; // lazily constructed
+  const form = html`<form>${Object.values(inputs)}`;
+  form.addEventListener("input", () => value = undefined);
+  return Object.defineProperty(form, "value", {
+    get() {
+      return value === undefined
+        ? (value = Object.fromEntries(Object.entries(inputs).map(([name, {value}]) => [name, value])))
+        : value;
+    },
+    set(v = {}) {
+      value = undefined;
+      for (const name in inputs) {
+        inputs[name].value = v[name];
+      }
+    }
+  });
 }
