fc7ef5d489bc19c745a7e8ebf43bd660dc1ef9a4
John Alexis Guerra Gómez <john.guerra@gmail.com>
2022-06-03 12:23:54 -0700

    Adding a full option to Input.search for searching the whole text, not just the beginning

--- test/search-test.js
+++ test/search-test.js
@@ -11,3 +11,11 @@ it("Inputs.search(data) supports Cyrillic symbols", () => {
   const s = Inputs.search(["red", "дом", "чудо", "Река Дон"], {query: "до"});
   assert.deepStrictEqual(s.value, ["дом", "Река Дон"]);
 });
+
+it("Inputs.search(data) supports full search", () => {
+  const s = Inputs.search(["red", "blue", "The Real", "Mr.Real", "un-real"], {query: "re", full:true});
+  assert.deepStrictEqual(s.value, ["red", "The Real", "Mr.Real", "un-real"]);
+
+  const s2 = Inputs.search(["red", "дом", "чудо", "Река Дон"], {query: "до", full:true});
+  assert.deepStrictEqual(s2.value, ["дом", "чудо", "Река Дон"]);
+});
