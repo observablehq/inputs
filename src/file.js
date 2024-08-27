7ae53c757080b99ca3b498653cf2bc49bf8125a1
Mike Bostock <mbostock@gmail.com>
2024-08-27 09:08:13 -0700

    Vanilla Inputs.file (#266)
    * pass file.size to AbstractFile
    
    * 0.11.1
    
    * excise LocalFile implementation
    
    * fix file test

--- src/file.js
+++ src/file.js
@@ -3,57 +3,38 @@ import {maybeWidth} from "./css.js";
 import {maybeLabel} from "./label.js";
 import {createText} from "./text.js";
 
-export function fileOf(AbstractFile) {
-
-  class LocalFile extends AbstractFile {
-    constructor(file) {
-      super(file.name, file.type, file.lastModified);
-      Object.defineProperty(this, "_", {value: file});
-      Object.defineProperty(this, "_url", {writable: true});
-    }
-    async url() {
-      return this._url || (this._url = URL.createObjectURL(this._));
-    }
-    async blob() {
-      return this._;
-    }
-    async stream() {
-      return this._.stream();
-    }
-  }
-
-  return function file({
-    label,
-    required,
-    accept,
-    capture,
-    multiple,
-    disabled,
-    width,
-    value, // eslint-disable-line no-unused-vars
-    submit, // eslint-disable-line no-unused-vars
-    ...options
-  } = {}) {
-    const input = html`<input
-      type=file
-      name=file
-      disabled=${disabled}
-      required=${required}
-      accept=${accept}
-      capture=${capture}
-      multiple=${multiple}
-    >`;
-    const form = html`<form class=__ns__ style=${maybeWidth(width)}>
-      ${maybeLabel(label, input)}<div class=__ns__-input>
-        ${input}
-      </div>
-    </form>`;
-    return createText(form, input, undefined, options, {
-      get: (input) => multiple ? Array.from(input.files, file => new LocalFile(file))
-        : input.files.length ? new LocalFile(input.files[0])
-        : null,
-      set: () => {}, // ignored
-      same: () => false // ignored
-    });
-  };
+export function file({
+  label,
+  required,
+  accept,
+  capture,
+  multiple,
+  disabled,
+  width,
+  value, // eslint-disable-line no-unused-vars
+  submit, // eslint-disable-line no-unused-vars
+  transform = (file) => file,
+  ...options
+} = {}) {
+  const input = html`<input
+    type=file
+    name=file
+    disabled=${disabled}
+    required=${required}
+    accept=${accept}
+    capture=${capture}
+    multiple=${multiple}
+  >`;
+  const form = html`<form class=__ns__ style=${maybeWidth(width)}>
+    ${maybeLabel(label, input)}<div class=__ns__-input>
+      ${input}
+    </div>
+  </form>`;
+  return createText(form, input, undefined, options, {
+    get: (input) => multiple ? Array.from(input.files, (file) => transform(file))
+      : input.files.length ? transform(input.files[0])
+      : null,
+    set: () => {}, // ignored
+    same: () => false // ignored
+  });
 }
